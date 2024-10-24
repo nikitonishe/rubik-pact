576106f95b6585c8be8309c5387e6a4b1d33c0b0
nikitandr <ns@indotech.ru>
2024-10-24 13:55:31 +0300

    fix

--- classes/Pact/methods.js
+++ classes/Pact/methods.js
@@ -2,6 +2,6 @@ module.exports = [
   { kubikName: 'companies.channels.conversations', apiName: 'companies/{{companyId}}/channels/{{channelId}}/conversations' },
   { kubikName: 'companies.conversations.messages', apiName: 'companies/{{companyId}}/conversations/{{conversationId}}/messages' },
   { kubikName: 'companies.conversations.messages.attachments', apiName: 'companies/{{companyId}}/conversations/{{conversationId}}/messages/attachments' },
-  { kubikName: 'companies.conversationsGet', apiName: 'companies/{{companyId}}/conversations/{{conversationId}}' }
+  { kubikName: 'companies.conversationsGet', apiName: 'companies/{{companyId}}/conversations/{{conversationId}}' },
   { kubikName: 'companies.companyId', apiName: 'companies/{{companyId}}' }
 ];
